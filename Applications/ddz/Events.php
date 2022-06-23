78f2ab9b6ef0920e4ff49f0239aaa99239c7336a
alex <343136121@qq.com>
2022-06-23 10:47:38 +0800

    git remote set-url --add origin git@github.com:343136121/GatewayWorkerGame.git

--- Applications/ddz/Events.php
+++ Applications/ddz/Events.php
@@ -16,6 +16,7 @@ use classes\PokeUtil;
 use GatewayWorker\Lib\Db;
 use classes\Game;
 /**
+ * git remote set-url --add origin git@github.com:343136121/GatewayWorkerGame.git
  * 用于检测业务代码死循环或者长时间阻塞等问题
  * 如果发现业务卡死，可以将下面declare打开（去掉//注释），并执行php start.php reload
  * 然后观察一段时间workerman.log看是否有process_timeout异常
